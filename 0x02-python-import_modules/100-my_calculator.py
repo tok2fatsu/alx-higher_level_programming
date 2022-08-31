df47aa87f7e326d8b878f367e048d38d47491454
tok2fatsu <mailit2moi@gmail.com>
2022-08-31 22:49:37 +0000

    Task 6: A program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-
-def new_cal():
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    length = len(argv) - 1
-    if (length < 3):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    oprtr = argv[2]
-
-    if oprtr not in ops:
-        print("Unknown operator. available operators: +, -, *, /")
-        exit(1)
-    result = ops[oprtr](a, b)
-    print("{} {} {} = {}".format(a, oprtr, b, result))
-
-
 if __name__ == "__main__":
+    import sys
     from calculator_1 import add, sub, mul, div
-    from sys import argv
-    new_cal()
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
