a73dbfd875c31862665a7279a71c19b1947f037d
tok2fatsu <mailit2moi@gmail.com>
2022-08-31 17:48:00 +0000

    Task 12: A function that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = 0
-    p = 0
-    if type(roman_string) is str and roman_string:
-        for c in range(len(roman_string) - 1, -1, -1):
-            if val[roman_string[c]] >= p:
-                res += val[roman_string[c]]
-            else:
-                res -+ val[roman_string[c]]
-            p = val[roman_string[c]]
-    return res
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    if (type(roman_string) is not str or roman_string is None):
+        return (0)
+    for i, char in enumerate(roman_string):
+        tmp_val = roman[char]
+        try:
+            if tmp_val < roman[roman_string[i + 1]]:
+                tmp_val = tmp_val * -1
+        except IndexError:
+            pass
+        num = num + tmp_val
+    return (num)
