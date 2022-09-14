58bce1bc748e6ebfe84a5956546855142841668a
tok2fatsu <mailit2moi@gmail.com>
2022-09-14 00:43:31 +0000

    Task 6: A class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 5-square
+"""Module 6-square
 This Module contains an definition for Square class
 """
 
@@ -7,12 +7,13 @@ This Module contains an definition for Square class
 class Square:
     """A class that represents a square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """initializes a square with size
         Args:
             size (int, optional): the size of the square. Defaults to 0.
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -37,6 +38,33 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """position property of the square
+        Returns:
+            tuple: pair of two positive numbers
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter for the position property
+        Args:
+            value (tuple): pair of two positive integers
+        Raises:
+            TypeError: if value is not tuple or the numbers are negative
+        """
+        if (
+            type(value) is not tuple
+            or len(value) != 2
+            or type(value[0]) is not int
+            or type(value[1]) is not int
+            or value[0] < 0
+            or value[1] < 0
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """calculates the area of the square
         Returns:
@@ -49,5 +77,8 @@ class Square:
         if (self.size == 0):
             print("")
         else:
+            for _ in range(self.position[1]):
+                print("")
             for _ in range(self.size):
+                print(*[' ' for _ in range(self.position[0])], sep='', end='')
                 print(*["#" for _ in range(self.size)], sep='')
