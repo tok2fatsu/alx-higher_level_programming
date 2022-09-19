c3420f58dc5fc85be2fa6de38adc02f281b706e5
tok2fatsu <mailit2moi@gmail.com>
2022-09-19 06:59:08 -0700

    Task 0: An empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""This module creates a rectangle"""
+"""""This module create a rectangle"""
+
 
 class Rectangle:
     """
-    pass attribute -empty class
+    pass atribute - empty class
     """
     def __init__(self):
-       pass
+        pass
