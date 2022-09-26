5187ab909589845e6b7cc25c3b1dd361da24f1f1
tok2fatsu <mailit2moi@gmail.com>
2022-09-26 14:27:38 -0700

    Task 7: A class BaseGeometry (based on 6-base_geometry.py)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,6 +16,6 @@ class BaseGeometry:
             Validating the integer
         '''
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
