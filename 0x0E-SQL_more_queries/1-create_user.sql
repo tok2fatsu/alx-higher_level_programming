3ddc31ce570897be7dd12520708db5fe7c823d35
tok2fatsu <mailit2moi@gmail.com>
2022-11-02 14:21:37 -0700

    Task 1: fixing bug

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
 --Creates a user named user_0d_1 and grants all previlages
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost;
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILAGES
+	ON *.*
+	TO 'user_0d_1'@'localhost';
