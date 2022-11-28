e28e89a93a54183b1a77ad4b7d5ef250509cd984
tok2fatsu <mailit2moi@gmail.com>
2022-11-28 21:02:25 +0000

    Task 7: A script that prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
 }
