c9861620310e7eafbb441b59799c7231c1e945f3
tok2fatsu <mailit2moi@gmail.com>
2022-11-28 19:22:46 +0000

    Task 1: A script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const first_line = 'C is fun';
-const second_line = 'Python is cool';
-const third_line = 'JavaScript is amazing';
-console.log(first_line);
-console.log(second_line);
-console.log(third_line);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
