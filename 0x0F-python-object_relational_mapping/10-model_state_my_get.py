91e1546da9e5ca94301fe51278ca9126728ce412
tok2fatsu <mailit2moi@gmail.com>
2022-12-04 08:19:55 -0800

    Task 10: A script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """
-Contains a script to get a state name
+Lists the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+Usage: ./10-model_state_my_get.py <mysql username> /
+                                  <mysql password> /
+                                  <database name>
+                                  <state name searched>
 """
-from sys import argv
-
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-
-def get_state_by_name(session, state_name):
-    """
-    Gets state by name
-
-    Args:
-        session (Session): sqlalchemy session
-        state_name (str): the name of the state
-    """
-    instance = session.query(State.id).filter(
-            State.name == state_name).first()
-
-    if instance is None:
-        print("Not found")
-    else:
-        print(instance.id)
-
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3], pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    get_state_by_name(session, state_name=argv[4])
+
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
