a3a62d78e31cb1b8ee55f767dc692601653b6502
tok2fatsu <mailit2moi@gmail.com>
2023-01-17 15:50:34 +0000

    Task 1: A script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(file, stringl, 'utf-8', function (err) {
+fs.writeFile(file, string, 'utf-8', function (err) {
   if (err) console.log(err);
 });
