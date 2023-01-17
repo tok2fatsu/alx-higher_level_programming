17d2aae1038dfbd73c80997b4e18aacab0a360d5
tok2fatsu <mailit2moi@gmail.com>
2023-01-17 16:26:12 +0000

    fixing bug

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ const url = process.argv[2];
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
     let count = 0;
     for (const filmIndex in films) {
